ac92473b2564f2861267335afc672eeea4714f54
Katy Daviscourt and Nicole Sanders <katy+nicole@>
2017-02-07 14:15:55 -0800

    fix errors

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -1,5 +1,5 @@
 using Nancy;
-using Cars.Objects;
+using Jobs.Objects;
 using System.Collections.Generic;
 
 namespace JobBoards
@@ -8,13 +8,13 @@ namespace JobBoards
   {
     public HomeModule()
     {
-      Get['/'] = _ => View["add_new_job.cshtml"];
-      Get['/view_all_jobs'] = _ => {
+      Get["/"] = _ => View["add_new_job.cshtml"];
+      Get["/view_all_jobs"] = _ => {
         List<Job> allJobs = Job.GetAll();
         return View["view_all_jobs.cshtml", allJobs];
       };
       Post["/jobs_added"] = _ => {
-        Job newJob = new Car(Request.Form["new-title"], Request.Form["new-description"], Request.Form["new-contact-info"]);
+        Job newJob = new Job(Request.Form["new-title"], Request.Form["new-description"], Request.Form["new-contact-info"]);
         newJob.Save();
         return View["jobs_added.cshtml", newJob];
       };
